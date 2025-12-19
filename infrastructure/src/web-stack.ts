018375b0cc21b72d0bdf0039e0f61f81b45a994f
Julian Burr <hello@julianburr.de>
2025-12-20 01:22:10 +1000

    wip

--- infrastructure/src/web-stack.ts
+++ infrastructure/src/web-stack.ts
@@ -1,21 +1,26 @@
+import * as crypto from 'node:crypto';
+import * as fs from 'node:fs';
 import * as path from 'node:path';
 
 import * as cloudflare from '@pulumi/cloudflare';
 import * as pulumi from '@pulumi/pulumi';
+
+function sha256File(filePath: string) {
+  const buf = fs.readFileSync(filePath);
+  return crypto.createHash('sha256').update(buf).digest('hex');
+}
 
 const accountId = process.env.CLOUDFLARE_ACCOUNT_ID;
 if (!accountId) {
   throw new Error('CLOUDFLARE_ACCOUNT_ID is not set');
 }
 
-const stack = pulumi.getStack();
-
 const zoneId = process.env.CLOUDFLARE_ZONE_ID;
 const routePattern = process.env.CLOUDFLARE_ROUTE_PATTERN;
-const scriptName = `boilerplate-app-web-${stack}`;
+const scriptName = 'boilerplate-app-web';
 
-const mainModule = 'workers/app.ts';
-const contentFile = path.resolve(__dirname, `../apps/web/${mainModule}`);
+const mainModule = 'workers/placeholder.js';
+const contentFile = path.resolve(__dirname, `../../apps/web/${mainModule}`);
 
 // Create the Worker once (placeholder code)
 const worker = new cloudflare.WorkersScript(
@@ -25,6 +30,7 @@ const worker = new cloudflare.WorkersScript(
     scriptName,
     mainModule,
     contentFile,
+    contentSha256: sha256File(contentFile),
     compatibilityDate: '2025-12-01',
   },
   // Let Wrangler deploy code without Pulumi trying to revert it
